e7f6855f5b58b97421f6decf1e4d4320fe747a1b
Vit Pavlik <pavlikvitek13@gmail.com>
2023-04-04 14:43:03 +0200

    more tests for tail

--- tests/test_tail.py
+++ tests/test_tail.py
@@ -1,7 +1,10 @@
 # test the first part of the project - utility `tail`
 import testutils as tu
+import os
 
 COMMAND="tail"  # command to be tested
+LINES_NUM = 99_999  # number of lines with whitch test for long inputs
+TMP_FILE_PATH = "test_tail_tmp.txt"
 
 
 def test_input(start: int, end: int) -> str:
@@ -49,10 +52,107 @@ def test3() -> None:
     tu.print_result(f"tail: {length:_} lines", conditions, result)
 
 
+def test4() -> None:
+    """tail: basic functionality with file"""
+    
+    # create a test input file
+    with open(TMP_FILE_PATH, "w") as f:
+        f.write(test_input(1, 15)) 
+    
+    result = tu.run(f"{COMMAND} {TMP_FILE_PATH}")
+
+    os.unlink(TMP_FILE_PATH)  # delete a test input file
+
+    conditions = [
+        result.rcode == 0,
+        result.stderr == "",
+        result.stdout == test_input(6, 15)
+    ]
+
+    tu.print_result("tail: basic functionality with file", conditions, result)
+
+
+def test5() -> None:
+    """tail: empty input file"""
+
+    # create a test file
+    with open(TMP_FILE_PATH, "w") as f:
+        f.write("")
+    
+    result = tu.run(f"{COMMAND} {TMP_FILE_PATH}")
+
+    os.unlink(TMP_FILE_PATH)
+
+    conditions = [
+        result.rcode == 0,
+        result.stderr == "",
+        result.stdout == ""
+    ]
+
+    tu.print_result("tail: empty input file", conditions, result)
+
+
+def test6() -> None:
+    """tail: input file with 99 999 lines"""
+    
+    number_of_lines = 99_999
+    
+    with open(TMP_FILE_PATH, "w") as f:
+        f.write(test_input(1, number_of_lines))
+    
+    result = tu.run(f"{COMMAND} {TMP_FILE_PATH}")
+
+    os.unlink(TMP_FILE_PATH)
+
+    conditions = [
+        result.rcode == 0,
+        result.stderr == "",
+        result.stdout == test_input(number_of_lines - 9, number_of_lines)
+    ]
+
+    tu.print_result(f"tail: file with {number_of_lines} lines", conditions, 
+                    result)
+
+
+def test7() -> None:
+    """tail: invalid number of lines for `-n` option"""
+
+    # \u011b LATIN SMALL LETTER E WITH CARON
+    invalid_nums = "a ff ` , : . / * - + \u011b"
+
+    rcodes = []
+    stdouts = []
+    stderrs = []
+    
+    for invalid_num in invalid_nums.split(" "):
+        result = tu.run(f"{COMMAND} -n {invalid_num}", 
+                        input_str=test_input(1, 15))
+        rcodes.append(result.rcode)
+        stdouts.append(result.stdout)
+        stderrs.append(result.stderr)
+
+    conditions = [rcode != 0 for rcode in rcodes]
+    conditions.extend([len(stderr) > 0 for stderr in stderrs])
+    conditions.extend([stdout == "" for stdout in stdouts])
+
+    # here the last result_t is used if the test fails even though
+    # it may not be the reason it failed
+    tu.print_result("tail: invalid number of lines for `-n` option", conditions, 
+                    result)
+
+
+
 def main():
     test1()
     test2()
     test3()
+    test4()
+    test5()
+    test6()
+    test7()
+    # doplnit testy:
+    #    kdyz je jako argument prepinace -n neco co neni cislo
+    #    kdyz je radek delsi nez implementacni limit
     
 
 if __name__ == "__main__":
