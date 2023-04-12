5872efe2cb843528fd6bb849d7d75747438050a1
Vit Pavlik <pavlikvitek13@gmail.com>
2023-04-12 13:49:34 +0200

    add htab_priv.h (private header for the library)

---
+++ htab_priv.h
@@ -0,0 +1,24 @@
+/*****************
+**  Vit Pavlik  **
+**   xpavli0a   **
+**    251301    **
+**              **
+**   Created:   **
+**  2023-04-12  **
+**              **
+** Last edited: **
+**  2023-04-12  **
+*****************/
+// Fakulta: FIT VUT
+// Vyvíjeno s gcc 10.2.1 na Debian GNU/Linux 11
+
+#include "htab.h"
+
+#ifndef HTAB_H_PRIV__
+#define HTAB_H_PRIV__
+
+struct htab {
+    // jo sem by to neco chtelo no
+};
+
+#endif  // #ifndef HTAB_H_PRIV__
